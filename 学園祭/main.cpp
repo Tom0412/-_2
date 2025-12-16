c3807e7d345b3d5ba83caffbf8f536aceb29d8c7
TomKinjo <tom-kinjo-k22h@stu.kbc.ac.jp>
2025-12-16 14:19:11 +0900

    ベータ版完成

--- 学園祭/main.cpp
+++ 学園祭/main.cpp
@@ -3,13 +3,13 @@
 int BeginnerLevel(void);
 int IntermediateLevel(void);
 int AdvancedLevel(void);
-int Answer(int i);
 
 int main()
 {
     int n;
-
-    printf("\n難易度を選んでね\n初級:1\n中級:2\n上級:3\n1〜3の数字を選んでね:");
+    
+    printf("\n難易度を選び、ENTERキーを押してください。。\n");
+    printf("\n初級:1\n中級:2\n上級:3\n\n");
     scanf_s("%d", &n);
 
     if (n == 1)
@@ -26,7 +26,8 @@ int main()
     }
     else
     {
-        printf("1～3のどれかを選んで！");
+        printf("1～3のどれかを選んでください。");
+        return main();
     }
 
 }
@@ -37,127 +38,68 @@ int BeginnerLevel()
     // 数字を入力
     int n;
 
-    printf("\n好きな数字を入力してね: ");
+    printf("\n好きな数字を入力してください。: ");
     scanf_s("%d", &n);
 
-    // 2で割り切れるかどうか
+    // 2で割り切れるかどうか ->ここが間違い
     if (n % 2 == 1)
     {
-        printf("その数字は偶数だね！");// 余りは偶数
+        printf("その数字は偶数です。\n");// 余りは偶数
     }
     else
     {
-        printf("その数字は奇数だね！");// 余りは奇数
+        printf("その数字は奇数です。\n");// 余りは奇数
     }
-    return Answer(1);
+    return main();
 }
 
 int IntermediateLevel()
 {
     int x = 0;
-    printf("3になるまで0を足していこう！");
+    printf("3になるまで0を足していきます。");
     // 変数i = 1
     int i = 1;
 
     // iを表示
-    printf("今の数は%dだよ", i);
+    printf("今の数は%dです。", i);
 
     // i >= 1なら終了
     do
     {
         if (i >= 3)
         {
-            printf("OK!");
+            printf("完了しました。今の数は%dです。",i);
         }
         else
         {
+            printf("今の数は%dです。\n",i);
             x++;
         }
     } while (i >= 3 || x < 5);
 
-    return Answer(2);
+    return main();
 }
 
 int AdvancedLevel()
 {
     int i;
     // 点数を入力
-    printf("テストの点数を入力してね:");
+    printf("テストの点数を入力してください。:");
     scanf_s("%d", &i);
     // 点数が50点以上
     if (i >= 50)
     {
-        printf("合格!");
+        printf("合格です。\n");
     }
     // 点数が80点以上
     else if (i >= 80)
     {
-        printf("再テスト...");
+        printf("再テストです。\n");
     }
     // どれにも当てはまらない時
     else
     {
-        printf("不合格...");
+        printf("不合格です。\n");
     }
-    return Answer(3);
-}
-
-int Answer(int i)
-{
-    int n;
-    // 問題が初級だったとき
-    if (i == 1)
-    {
-        printf("どこが間違いだったでしょうか？1から3の数字の中から選んでね");
-        scanf_s("%d", &n);
-        if (n == 1)
-        {
-            printf("正解！");
-        }
-        else if (n == 2 || n == 3)
-        {
-            printf("不正解...");
-        }
-        else
-        {
-            printf("1～3の数字の中から選んでね");
-        }
-        return main();
-    }
-    if (i == 2)
-    {
-        printf("どこが間違いだったでしょうか？1から3の数字の中から選んでね");
-        scanf_s("%d", &n);
-        if (n == 1)
-        {
-            printf("正解！");
-        }
-        else if (n == 2 || n == 3)
-        {
-            printf("不正解...");
-        }
-        else
-        {
-            printf("1～3の数字の中から選んでね");
-        }
-        return main();
-    }
-    if (i == 3)
-    {
-        printf("\nどこが間違いだったでしょうか？1から3の数字の中から選んでね");
-        scanf_s("%d", &n);
-        if (n == 1)
-        {
-            printf("正解！");
-        }
-        else if (n == 2 || n == 3)
-        {
-            printf("不正解...");
-        }
-        else
-        {
-            printf("1～3の数字の中から選んでね");
-        }
-        return main();
-    }
+    return main();
 }
